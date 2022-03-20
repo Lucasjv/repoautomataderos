f0c109baca4d2c402924e8c9e659a8ce483f6cb2
Lucas Joaquin Vanini <ljvanini92@gmail.com>
2022-03-20 11:19:01 -0300

    script

---
+++ script.js
@@ -0,0 +1,41 @@
+function saludar ()
+{alert ("Gracias por visitar nuestra página, recuerde que nuestros horarios de atención son de 8 a 13hs y 14 a 18hs, teléfono 4683-7070")}
+
+let nombre = prompt ("Ingrese por favor su nombre y apellido")
+{ 
+    alert ("Bienvenido " + nombre);
+    console.log (nombre)
+}
+
+const claveOk = 1234
+
+let clave = parseInt(prompt ("Ingrese su clave"))
+
+if ( clave === claveOk)
+{alert ("Ingreso Exitoso");
+}
+else { alert ("ingreso no exitoso"); 
+}
+
+
+if  (isNaN(clave)) { alert ("Su clave solo debe ser numérica");}
+
+saludar ()
+
+let fecha = prompt ("Ingrese qué día de la semana desea solicitar turno")
+{ console.log (fecha)}
+
+let turnos = parseInt(prompt("Ingrese la cantidad de vehículos que quiera traer"))
+
+let disponibles = "20";
+
+if (turnos <= disponibles )
+{ alert ("¡turno confirmado con éxito! Complete el formulario de contacto y nos estaremos comunicando a la brevedad");}
+else { alert (" No quedan turnos para esta semana");}
+
+saludar ()
+
+//*turnos que quedan disponibles por día*//
+
+for (let i = turnos ; i<=disponibles ; i++)
+{console.log (i);}

ea25fbb049f9fcc9cb0d8a9d1c83af3054ac57ba
Lucas Joaquin Vanini <ljvanini92@gmail.com>
2022-03-24 18:26:09 -0300

    Entrega arrays

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function saludar ()
 {alert ("Gracias por visitar nuestra página, recuerde que nuestros horarios de atención son de 8 a 13hs y 14 a 18hs, teléfono 4683-7070")}
 
@@ -20,7 +21,6 @@ else { alert ("ingreso no exitoso");
 
 if  (isNaN(clave)) { alert ("Su clave solo debe ser numérica");}
 
-saludar ()
 
 let fecha = prompt ("Ingrese qué día de la semana desea solicitar turno")
 { console.log (fecha)}
@@ -39,3 +39,52 @@ saludar ()
 
 for (let i = turnos ; i<=disponibles ; i++)
 {console.log (i);}
+
+
+
+
+const vehiculos = ["ka 2017", "Ranger", "focusiv", "fiesta", "Ecosport", "mondeo", "bronco", "maverik", "territory", "Transit"];
+
+console.log (vehiculos)
+
+let modelo = prompt ("Ingrese el modelo de su vehículo para conocer si disponemos de equipamiento");
+
+const indice = vehiculos.indexOf(modelo);
+
+if (indice!==-1)
+
+{
+    alert (`Disponemos de equipamiento y repuestos para su ${vehiculos[indice]}`)  
+}
+
+else 
+
+{
+    alert ("Su vehículo ya ha sido discontinuado por fábrica y no disponemos de equipamiento")
+};
+
+
+function Producto (nombre, precio, mo, promo)
+
+{
+
+    this.nombre= nombre
+    this.precio= precio
+    this.mo= mo
+    this.promo= promo
+
+
+}
+
+
+const tuercas = new Producto ("Tuercas de seguridad", 9600, 0, false)
+const sensores = new Producto ("Sensor de estacionamiento sonoro", 9600, 4200, false)
+const camara = new Producto ("camara de retroceso", 10200, 3500, false)
+const chapon = new Producto ("chapon protector de carter y caja", 13500, 0, true)
+const alarma = new Producto ("Alarma volumétrica", 12500, 2800, false)
+
+console.log(tuercas)
+console.log(sensores)
+console.log(camara)
+console.log(chapon)
+console.log(alarma)

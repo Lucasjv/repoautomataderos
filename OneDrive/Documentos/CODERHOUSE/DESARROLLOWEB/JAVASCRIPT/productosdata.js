3315aa855be8de63126a0f23bf0ca4aab64f96cb
Lucas Joaquin Vanini <ljvanini92@gmail.com>
2022-04-17 20:19:29 -0300

    pruebas js

---
+++ OneDrive/Documentos/CODERHOUSE/DESARROLLOWEB/JAVASCRIPT/productosdata.js
@@ -0,0 +1,39 @@
+const productos = [
+
+
+    {id:01, nombre: "Chapon protector de carter y caja", precio: 10200, categoria: 1, stock:10 },
+    {id:02, nombre: "Tuercas de seguridad", precio: 10500, categoria: 1, stock: 10 },
+    {id:03, nombre: "Sensor de estacionamiento sonoro", precio: 13200, categoria: 1, stock:10 },
+    {id:04, nombre: "Cámara retroceso", precio: 12300, categoria: 1, stock:10 },
+    {id:05, nombre: "Alarma volumétrica", precio: 12100, categoria: 1, stock:10 },
+ 
+ 
+    {id:06, nombre: "polarizado", precio: 8200, categoria: 5, stock: 10 },
+    {id:07, nombre: "tratamiento de teflón", precio: 9800, categoria: 5, stock:10 },
+    {id:08, nombre: "Polarizado antivandálico", precio: 12500, categoria: 5, stock:10 },
+ 
+ 
+    {id:09, nombre: "Lona marítima", precio: 35500, categoria: 2, stock: 10 },
+    {id:10, nombre: "Gancho de remolque + instalación eléctrica", precio: 62500, categoria: 2, stock:10 },
+    {id:11, nombre: "Cobertor de caja", precio: 44600, categoria: 2, stock:10 },
+    {id:12, nombre: "Sensor de estacionamiento sonoro", precio: 15300, categoria: 2, stock:10 },
+ 
+    {id:13, nombre: "Chapon protector de carter y caja",  precio: 10200, categoria: 4, stock:10},
+    {id:14, nombre: "Tuercas de seguridad", precio: 10500, categoria: 4, stock: 10 },
+    {id:15, nombre: "Sensor de estacionamiento sonoro", precio: 13200, categoria: 4, stock:10 },
+    {id:16, nombre: "Cámara retroceso", precio: 12300, categoria: 4, stock:10 },
+ 
+ 
+    {id:17, nombre: "Tuercas de seguridad", precio: 5900, categoria: 6, stock:10 },
+
+
+   {id:18, nombre: "Chapon protector de carter y caja", precio: 10200, categoria: 3, stock: 10 },
+   {id:19, nombre: "Sensor de estacionamiento sonoro", precio: 15300, categoria: 3, stock: 10 },
+   {id:20, nombre: "Cámara retroceso", precio: 12300, categoria: 3, stock: 10  },
+   {id:21, nombre: "Cobertor de rueda de auxilio pintado al tono carrocería", precio: 33500, categoria: 3, stock: 10 },
+   {id:22, nombre: "Barras portaequipaje", precio: 15300, categoria: 3, stock: 0 }
+
+]
+
+  
+
